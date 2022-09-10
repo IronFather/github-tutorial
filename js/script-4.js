310f1334384b0a4c7b7232ebb5e40934ef855d54
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-09-10 08:35:40 +0300

    started work with tasks of new module 4

---
+++ js/script-4.js
@@ -0,0 +1,21 @@
+// ПЕРЕБИРАЮЩИЕ МЕТОДЫ
+
+// map()
+const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+
+const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
+console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПИТЕР']
+
+const planetsInLowerCase = planets.map(planet => planet.toLowerCase());
+console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
+
+// Оригинальный массив не изменился
+console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпитер']
+
+// flatMap()
+
+
+
+
+
+// =================================================================================================
