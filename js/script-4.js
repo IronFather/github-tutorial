01746eedb7b317db550bfeb6ea795bf7e6d04952
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-09-14 23:20:27 +0300

    Update script-4.js

--- js/script-4.js
+++ js/script-4.js
@@ -134,14 +134,196 @@
 // стрелки не бывают методом объекта
 // ======================================================================================
 
+// FOREACH
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// numbers.forEach(function (number) {
+//     // funcion колбек для действий с КАЖДЫМ ЭЛЕМЕНТОМ после перебора
+//   console.log('number', number);
+// });
+
+// console.log(numbers);
+
+// у FOREACH есть два аргумента 1) CALLBACK и 2) ОБЪЕКТ в контексте которого будет вызываться этот колбек (thisArg)
+
+// ======================================================================================
+
+// MAP
+
+/*
+ * Array.prototype.map()
+ * - Поэлементо перебирает оригинальный массив
+ * - Не изменяет оригинальный массив
+ * - Возвращает новый массив такой же длины
+ */
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const doubledNums = numbers.map(number => {
+//   return number * 13;
+// })
+
+// console.log('numbers', numbers);
+// console.log('doubledNums', doubledNums);
+
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+// console.table(players);
+
+/*
+ * Получаем массив имён всех игроков
+ */
+
+// const playerNames = players.map(player => player.name);
+// console.log('playerNames', playerNames);
+
+// const playerIds = players.map(player => player.id);
+// // console.log('playerIds', playerIds);
+
+// const res = players.map(({ name, online }) => ({ name, online }));
+// console.log('res', res);
+
+/*
+ * Увеличиваем кол-во поинтов каждого игрока на 10%
+ */
+
+// const upatedPlayers = players.map(player => ({
+//   ...player,
+//   points: player.points * 1.1,
+// }));
+
+// console.table(upatedPlayers);
+// console.log(upatedPlayers);
+
+/*
+ * Увеличиваем кол-во часов игрока по id
+ */
+
+// const playerIdToUpdate = 'player-3';
+
+// const updatedPlayers = players.map(player => {
+//   if (playerIdToUpdate === player.id) {
+//     return {
+//       ...player,
+//       timePlayed: player.timePlayed + 320,
+//       points: player.points + 33,
+//     };
+//   }
+
+//   return player;
+// });
+
+// const updatedPlayers = players.map(player =>
+//   playerIdToUpdate === player.id
+//     ? { ...player, timePlayed: player.timePlayed + 100 }
+//     : player,
+// );
+// console.table(updatedPlayers);
+
+// ======================================================================================
+
+// FILTER
+// ИЩЕТ МНОГОЕ ИЗ МНОГИХ
+
+/*
+ * Array.prototype.filter()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает новый массив (с элементами или пустой)
+ * - Добавляет в возвращаемый массив элементы которые удовлетворяют условию коллбек-функции
+ *    - если коллбек вернул true элемент добавляется в возвращаемый массив
+ *    - если коллбек вернул false элемент НЕ добавляется в возвращаемый массив
+ */
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const filteredNumbers = numbers.filter(number => number < 10 || number > 20);
+// // console.log(filteredNumbers);
+
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
+
+/*
+//  * Получаем массив всех онлайн игроков
+//  */
+
+// const onlinePlayers = players.filter(({ online }) => online);
+// // console.table(onlinePlayers);
+
+// /*
+//  * Получаем массив всех оффлайн игроков
+//  */
+
+// const offlinePlayers = players.filter(player => !player.online);
+// // console.table(offlinePlayers);
+
+// /*
+//  * Получаем список хардкорных игроков с временем больше 250
+//  */
+
+// const hardcorePlayers = players.filter(player => player.timePlayed > 250);
+// // console.table(hardcorePlayers);
+
+// ======================================================================================
+
+// FIND
+//  ИЩЕТ ОДИН ЭЛЕМЕНТ (ПЕРВЫЙБ ВЫПОЛНЯЮЩИЙ УСЛОВИЕ)
+/*
+ * Array.prototype.find()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает первый элемент удовлетворяющий условию или undefined
+ */
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const number = numbers.find(number => number === 10);
+// // console.log(number);
+
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
+
+// /*
+//  * Ищем игрока по id
+//  */
+// const playerIdToFind = 'player-3';
+// const playerWithId = players.find(({ id }) => id === playerIdToFind);
+// // console.log(playerWithId);
+
+// const finPlayerById = (allPlayer, playerId) => {
+//   return allPlayer.find(({ id }) => id === playerId);
+// };
+
+// // console.log(finPlayerById(players, 'player-1'));
+// // console.log(finPlayerById(players, 'player-4'));
+
+// /*
+//  * Ищем игрока по имени
+//  */
+// const playerNameToFind = 'Poly';
+// const playerWithName = players.find(player => player.name === playerNameToFind);
+// // console.log(playerWithName);
+
+// ======================================================================================
 
 
 
 
-
-
-
-
+// ======================================================================================
 
 
 
