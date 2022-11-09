ded347723ddf276c20d686e5c4694a8c0953a9d4
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-09 21:37:43 +0200

    11

---
+++ js/JS_6.2/6.2.js
@@ -0,0 +1,80 @@
+// 1-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// const buttonEl = document.querySelector(`.my-button`);
+
+// console.log(buttonEl);
+
+// const clickToButton = () => {
+//     console.log(`Ой, мене натиснули`);
+// }
+
+// buttonEl.addEventListener(`click`, clickToButton);
+
+// 2-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+const singleBtn = document.querySelector(`#single`);
+
+console.log(singleBtn);
+
+const handleClick = () => {
+    console.log(`Не тисніть на мене`);
+};
+
+singleBtn.addEventListener(`click`, handleClick);
+
+// const handleClick = () => {
+//   console.log("click event listener callback");
+// };
+
+// singleBtn.addEventListener("click", handleClick);
+
+// ===============================================
+
+const multiBtn = document.querySelector(`#multiple`);
+console.log(multiBtn);
+
+const firstCb = () => {
+    console.log(`Перший пішов`);
+}
+
+const secondCb = () => {
+    console.log(`Припини!`);
+}
+
+const thirdCb = () => {
+    console.log(`Перший пішов`);
+}
+
+// const multiBtn = document.querySelector("#multiple");
+
+// const firstCallback = () => {
+//   console.log("First callback!");
+// };
+// const secondCallback = () => {
+//   console.log("Second callback!");
+// };
+// const thirdCallback = () => {
+//   console.log("Third callback!");
+// };
+
+// multiBtn.addEventListener("click", firstCallback);
+// multiBtn.addEventListener("click", secondCallback);
+// multiBtn.addEventListener("click", thirdCallback);
+
+// 3-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+
+// 4-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+
+// 5-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+
+// 6-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+
+// 7-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
--- js/dom_6_module/07-products.js
+++ js/dom_6_module/07-products.js
@@ -1,8 +1,40 @@
-import products from './data/proucts.js';
+// {
+//   name: 'Сервоприводы',
+//   description:
+//     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+//   price: 2000,
+//   available: true,
+//   onSale: true,
+// },
+// {
+//   name: 'Генератор поля',
+//   description:
+//     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+//   price: 3000,
+//   available: false,
+//   onSale: false,
+// },
+// {
+//   name: 'Нулевой элемент',
+//   description:
+//     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+//   price: 1500,
+//   available: true,
+//   onSale: false,
+// },
+// {
+//   name: 'Титаниум',
+//   description:
+//     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+//   price: 2500,
+//   available: false,
+//   onSale: false,
+// },
+// ];
 
-/*
- * Создаём карточку продукта
- * - В классе продукта может быть product--on-sale product--not-available
+// /*
+//  * Создаём карточку продукта
+//  * - В классе продукта может быть product--on-sale product--not-available
  */
 
 {
