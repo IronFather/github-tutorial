8311390dc343fdd449fabd62d7103b7b44eafd18
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-11 22:36:00 +0200

    14

--- js/JS_6.2/6.2.js
+++ js/JS_6.2/6.2.js
@@ -270,12 +270,425 @@
 
 // setOutput();
 
-// 7-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// input-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// const textInput = document.querySelector(".text-input");
+// const output = document.querySelector(".output");
+
+// textInput.addEventListener("input", (event) => {
+//   output.textContent = event.currentTarget.value;
+// });
+
+// focus и blur-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// const setFocusBtnEl = document.querySelector(`[data-action="set"]`);
+// const removeFocusBtnEl = document.querySelector(`[data-action="remove"]`);
+// const inputEl = document.querySelector(`text-input`);
+
+// setFocusBtnEl.addEventListener("click", () => {
+//   inputEl.focus();
+// });
+
+// removeFocusBtnEl.addEventListener("click", () => {
+//   inputEl.blur();
+// });
+
+// inputEl.addEventListener("focus", () => {
+//   inputEl.value = "Уся увага намені";
+// });
+
+// inputEl.addEventListener("blur", () => {
+//   inputEl.value = "";
+// });
 
 
+// const textInput = document.querySelector(".text-input");
+// const setFocusBtn = document.querySelector('[data-action="set"]');
+// const removeFocusBtn = document.querySelector('[data-action="remove"]');
 
-// 7-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// setFocusBtn.addEventListener("click", () => {
+//   textInput.focus();
+// });
+
+// removeFocusBtn.addEventListener("click", () => {
+//   textInput.blur();
+// });
+
+// textInput.addEventListener("focus", () => {
+//   textInput.value = "This input has focus";
+// });
+
+// textInput.addEventListener("blur", () => {
+//   textInput.value = "";
+// });
+
+// 01-event-listeners.js-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 
+/*
+ * События.
+ * - Создание и удаление слушателей
+ * - Именование колбеков для слушателей
+ *    - handle*: handleSubjectEvent
+ *    - *Handler: subjectEventHandler
+ *    - on*: onSubjectEvent
+ * - Ссылочная идентичность колбеков
+ * - Объект события
+ */
 
-// 7-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// const targetBtn = document.querySelector('.js-target-btn');
+// const addListenerBtn = document.querySelector('.js-add-listener');
+// const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+// addListenerBtn.addEventListener("click", event => {
+//     console.log(event);
+//     console.log('Вешая слушителя на кнопку');
+// });
+
+// addListenerBtn.addEventListener("click", () => {
+//     console.log(`вешаю слушителя на целевую кнопку`);
+
+//     targetBtn.addEventListener("click", () => {
+// console.log("клик по целевой кнопке");
+//     })
+// });
+
+// removeListenerBtn.addEventListener("click", () => {
+//     console.log(`убираю слушателя с целевой кнопки`);
+
+//     targetBtn.removeEventListener("click", () => {
+
+//     })
+// })
+
+// addListenerBtn.addEventListener("click", () => {
+//     console.log(`вешаю слушителя на целевую кнопку`);
+
+//     targetBtn.addEventListener("click", onTargetBtnClick)
+// });
+
+// removeListenerBtn.addEventListener("click", () => {
+//     console.log(`убираю слушателя с целевой кнопки`);
+
+//     targetBtn.removeEventListener("click", onTargetBtnClick);
+// });
+
+// function onTargetBtnClick(event) {
+//     console.log(`клик по целевой кнопке`);
+// }
+
+// ---------------------------------------------------
+
+// const targetBtn = document.querySelector('.js-target-btn');
+// const addListenerBtn = document.querySelector('.js-add-listener');
+// const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+// addListenerBtn.addEventListener('click', event => {
+//   console.log(event);
+//   console.log('Вешаю слушателя события на целевую кнопку');
+
+//   targetBtn.addEventListener('click', onTargetBtnClick);
+// });
+
+// removeListenerBtn.addEventListener('click', event => {
+//   console.log(event);
+//   console.log('Снимаю слушателя события с целевой кнопки');
+
+//   targetBtn.removeEventListener('click', onTargetBtnClick);
+// });
+
+// function onTargetBtnClick(event) {
+//   console.log(event);
+//   console.log('Клик по целевой кнопке');
+// }
+
+// 02-forms.js-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// const formEl = document.querySelector(`.js-register-form`);
+// // console.log(formEl);
+
+// formEl.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(event) {
+// event.preventDefault();
+
+// console.dir(event.currentTarget);
+// console.log(`это сабмит формы`);
+
+// const formElements = event.currentTarget.elements;
+
+// const formData = new FormData(event.currentTarget);
+
+// console.log(formData);
+// }
+
+// ---------------------------------------------------
+/*
+ * - Событие submit
+ * - Действия браузера по умолчанию
+ * - Свойство elements
+ * - Класс FormData - https://developer.mozilla.org/en-US/docs/Web/API/FormData
+ */
+
+// const form = document.querySelector(".js-register-form");
+
+// form.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   console.log(form.elements);
+
+//   const formData = new FormData(form);
+//   console.log(formData);
+
+//   formData.forEach((value, name) => {
+//     console.log("onFormSubmit -> name", name);
+//     console.log("onFormSubmit -> value", value);
+//   });
+// }
+
+// 03-input-events.js &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// const refs = {
+//     inputEl: document.querySelector(`.js-input`),
+//     spanEl: document.querySelector(`.js-button > span`),
+//     checkboxEl: document.querySelector(`.js-license`),
+//     btnEl: document.querySelector(`.js-button`),
+// }
+
+// refs.inputEl.addEventListener("focus", onInputFocus);
+// refs.inputEl.addEventListener("blur", onInputBlur);
+// refs.inputEl.addEventListener("change", onInputChange);
+// refs.inputEl.addEventListener("input", onInputChange);
+// refs.checkboxEl.addEventListener("change", onCheckboxChange);
+
+// function onInputFocus() {
+//     console.log(`фокус на импуте`);
+// }
+
+// function onInputBlur() {
+//     console.log(`фокус потерян`);
+// }
+
+// function onInputChange(event) {
+//     console.log(event.currentTarget.value);
+
+//     refs.spanEl.textContent = event.currentTarget.value;
+// }
+
+// function onCheckboxChange(event) {
+//     console.log(event.currentTarget.checked);
+//     console.log(`refs.btnEl.disabled :`, refs.btnEl.disabled); 
+
+//     refs.btnEl.disabled = !event.currentTarget.checked;
+// }
+
+// ---------------------------------------------------
+
+/*
+ * Паттерн «Объект ссылок»
+ *
+ * События
+ * - focus и blur
+ * - input и change
+ * - Чекбоксы и свойство checked
+ */
+
+// const refs = {
+//     input: document.querySelector('.js-input'),
+//     nameLabel: document.querySelector('.js-button > span'),
+//     licenseCheckbox: document.querySelector('.js-license'),
+//     btn: document.querySelector('.js-button'),
+//   };
+  
+//   // refs.input.addEventListener('focus', onInputFocus);
+//   // refs.input.addEventListener('blur', onInputBlur);
+//   // refs.input.addEventListener('change', onInputChange);
+//   // refs.input.addEventListener('input', onInputChange);
+  
+//   refs.input.addEventListener('input', onInputChange);
+//   refs.licenseCheckbox.addEventListener('change', onLicenseChange);
+  
+//   function onInputFocus() {
+//     console.log('Инпут получил фокус - событие focus');
+//   }
+  
+//   function onInputBlur() {
+//     console.log('Инпут потерял фокус - событие blur');
+//   }
+  
+//   function onInputChange(event) {
+//     refs.nameLabel.textContent = event.currentTarget.value;
+//   }
+  
+//   function onLicenseChange(event) {
+//     refs.btn.disabled = !event.currentTarget.checked;
+//   }
+
+// 04-keyboard-events.js-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// const refs = {
+//     outputEl: document.querySelector('.js-output'),
+//     clearBtnEl: document.querySelector('.js-clear'),
+// }
+
+// window.addEventListener(`keydown`, onKeyPress);
+
+// function onKeyPress(event) {
+    // console.log(event.key);
+    // console.log(event.code);
+
+    // refs.outputEl.textContent += event.key;
+
+// }
+
+// refs.clearBtnEl.addEventListener(`click`, onClearOutput);
+
+// function onClearOutput() {
+//     refs.outputEl.textContent = "";
+// }
+
+
+// ---------------------------------------------------
+
+/*
+ * Типы событий: keypress, keydown, keyup
+ * - Ограничения keypress
+ * - Свойства KeyboardEvent.key и KeyboardEvent.code
+ */
+
+// const refs = {
+//     output: document.querySelector('.js-output'),
+//     clearBtn: document.querySelector('.js-clear'),
+//   };
+  
+//   window.addEventListener('keypress', onKeypress);
+//   refs.clearBtn.addEventListener('click', onClearOutput);
+  
+//   function onKeypress(event) {
+//     // console.log('event.key: ', event.key);
+//     // console.log('event.code: ', event.code);
+  
+//     refs.output.textContent += event.key;
+//   }
+  
+//   function onClearOutput() {
+//     refs.output.textContent = '';
+//   }
+
+// 05-mouse-events.js-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+const boxEl = document.querySelector('.js-box');
+
+// boxEl.addEventListener('mouseenter', onMouseEnter);
+// boxEl.addEventListener('mouseleave', onMouseLeave);
+
+// function onMouseEnter(events) {
+// const box = event.currentTarget;
+// box.classList.add(`box--active`);
+// }
+
+// function onMouseLeave(event) {
+//     const box = event.currentTarget;
+//     box.classList.remove(`box--active`);
+//     }
+
+    // boxEl.addEventListener('mouseover', onMouseEnter);
+    // boxEl.addEventListener('mouseout', onMouseLeave);
+    
+    // function onMouseEnter(events) {
+    // const box = event.currentTarget;
+    // box.classList.add(`box--active`);
+    // }
+    
+    // function onMouseLeave(event) {
+    //     const box = event.currentTarget;
+    //     box.classList.remove(`box--active`);
+    //     }
+
+    // boxEl.addEventListener('mousemove', onMouseMove);
+    // function onMouseMove(event) {
+    //       console.log(event);
+    //     }
+
+// ---------------------------------------------------
+
+/*
+ * События мыши
+ * - mouseenter и mouseleave (это ховер через JS)
+ * - mouseover и mouseout
+ * - mousemove (chatty event - болтливое событие)
+ * - Допмат по координатам: https://nerdparadise.com/programming/javascriptmouseposition
+ * - Хорошая задачка - https://learn.javascript.ru/task/move-ball-field
+ */
+
+// const boxRef = document.querySelector('.js-box');
+
+// boxRef.addEventListener('mouseover', onMouseEnter);
+// boxRef.addEventListener('mouseout', onMouseLeave);
+// boxRef.addEventListener('mousemove', onMouseMove);
+
+// function onMouseEnter(event) {
+//   const box = event.currentTarget;
+//   box.classList.add('box--active');
+// }
+
+// function onMouseLeave(event) {
+//   const box = event.currentTarget;
+//   box.classList.remove('box--active');
+// }
+
+// function onMouseMove(event) {
+//   console.log(event);
+// }
+
+// 06-modal.js-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// ---------------------------------------------------
+
+/*
+ * 1. Открыть и закрыть по кнопке
+ * 2. Закрыть по клику в бекдроп: onBackDropClick
+ * 3. Закрыть по ESC: onEscapeKeypress
+ *
+ * В CSS есть класс show-modal, который необходимо добавить на body при открытии модалки
+ */
+
+// const refs = {
+//     openModalBtn: document.querySelector('[data-action="open-modal"]'),
+//     closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+//     backdrop: document.querySelector('.js-backdrop'),
+//   };
+  
+//   refs.openModalBtn.addEventListener('click', onOpenModal);
+//   refs.closeModalBtn.addEventListener('click', onCloseModal);
+//   refs.backdrop.addEventListener('click', onBackdropClick);
+  
+//   function onOpenModal() {
+//     window.addEventListener('keydown', onEscKeyPress);
+//     document.body.classList.add('show-modal');
+//   }
+  
+//   function onCloseModal() {
+//     window.removeEventListener('keydown', onEscKeyPress);
+//     document.body.classList.remove('show-modal');
+//   }
+  
+//   function onBackdropClick(event) {
+//     if (event.currentTarget === event.target) {
+//       console.log('Кликнули именно в бекдроп!!!!');
+//       onCloseModal();
+//     }
+//   }
+  
+//   function onEscKeyPress(event) {
+//     const ESC_KEY_CODE = 'Escape';
+//     const isEscKey = event.code === ESC_KEY_CODE;
+  
+//     if (isEscKey) {
+//       onCloseModal();
+//     }
+//   }
+
+// -&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// ---------------------------------------------------
