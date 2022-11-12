f87c70f64ebbc68c2b1d195b902ca68e63d088a1
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-12 13:12:12 +0200

    17

--- js/JS_6.2/6.2.js
+++ js/JS_6.2/6.2.js
@@ -483,6 +483,24 @@
 //     refs.btnEl.disabled = !event.currentTarget.checked;
 // }
 
+// const inputEl = document.querySelector(`.js-input`);
+
+// inputEl.addEventListener(`focus`, onFocusInput);
+// inputEl.addEventListener(`blur`, onBlurInput);
+// inputEl.addEventListener(`focus`, onTargetInput);
+
+// function onFocusInput() {
+//     console.log(`ФОКУС`); 
+// }
+ 
+// function onBlurInput(){
+//     console.log(`БЛУР`); 
+// }
+
+// function onTargetInput(event) {
+//     console.log(event.target.value);
+// }
+
 // ---------------------------------------------------
 
 /*
@@ -577,7 +595,7 @@
 //   }
 
 // 05-mouse-events.js-&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-const boxEl = document.querySelector('.js-box');
+// const boxEl = document.querySelector('.js-box');
 
 // boxEl.addEventListener('mouseenter', onMouseEnter);
 // boxEl.addEventListener('mouseleave', onMouseLeave);
@@ -737,6 +755,93 @@ const boxEl = document.querySelector('.js-box');
 //     }
 //   }
 
-// -&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// -6.2.-МОДАЛЬНОЕ ОКНО-ЗАНКИЕВ_ЗАДАЧА-1-&&&&&&&&&&&&&&&&&&&&&&
+
+const loginModalButton = document.querySelector("#login-button");
+const loginModal = document.querySelector(".modal-wrapper");
+const title = document.querySelector("h2");
+const error = document.querySelector(".form-error");
+const loginForm = document.querySelector("#login-form");
+const loginModalCloseButton = document.querySelector(
+	".modal-wrapper .close-button"
+);
+const loginModalOverlay = document.querySelector(".modal-wrapper .overlay");
+
+const onEscapePress = (e) => {
+	if (e.code === "Escape" && isLoginModalWindowOpen()) {
+		closeLoginModalWindow();
+	}
+};
+
+const closeLoginModalWindow = () => {
+	loginModal.classList.add("hidden");
+	document.removeEventListener("keyup", onEscapePress);
+	loginForm.reset();
+
+	if (!error.classList.contains("hidden")) {
+		error.classList.add("hidden");
+	}
+};
+
+const showLoginModalWindow = () => {
+	loginModal.classList.remove("hidden");
+
+	document.addEventListener("keyup", onEscapePress);
+};
+
+const isLoginModalWindowOpen = () => !loginModal.classList.contains("hidden");
+
+loginModalButton.addEventListener("click", showLoginModalWindow);
+
+loginModalCloseButton.addEventListener("click", closeLoginModalWindow);
+
+loginModalOverlay.addEventListener("click", closeLoginModalWindow);
+
+const correctLogin = "goit@gmail.com";
+const correctPasword = "1234";
+
+loginForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const data = new FormData(event.target);
+	const login = data.get("login");
+	const password = data.get("password");
+
+	if (login === correctLogin && password === correctPasword) {
+		closeLoginModalWindow();
+		title.classList.remove("hidden");
+		loginModalButton.classList.add("hidden");
+	} else {
+		error.classList.remove("hidden");
+	}
+
+	return data;
+});
+
+// =>
+// Задача 1.
+// Створити модальне вікно яке буде відкриватися
+// при кліку на кнопку всередині тега body
+// Модальне вікно має закриватися при кліку на напівпрозорий оверлей,
+// та на іконку хрестика всередині модалки
+
+// =>
+// Задача 2.
+// Додати в модальне вікно логіку закриття
+// при натисканні на кнопку "Escape"
+
+// =>
+// Задача 3.
+// Створити форму реєстрації в модалці користувача за допомогою js,
+// де буде 2 поля:
+//   1. Поле логіна
+//   2. Поле пароль
+// Захаркодити вірні логін і пароль в константах
+// Додати логіку сабміта форми, при сабміті перевіряємо введені логін і пароль.
+// Умови:
+//   1. Якщо логін і пароль співпадають,
+//     то видаляємо форму з документа і показуємо h2 з написом "Вхід успішний"
+//   2. Якщо логін і пароль не співпадають з даними з констант,
+//      то показуємо під формою помилку червоним кольором “Логін або пароль не вірні”
 
 // ---------------------------------------------------
